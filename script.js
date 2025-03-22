18996c94271774c68bb57f451aeee466c44a1e64
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-03-22 22:11:19 +0530

    added the function to save notes to local storage

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function newNote() {
   document.getElementById("noteTitle").value = "";
   document.getElementById("noteContent").value = "";
 }
+function saveNote() {
+    let title = document.getElementById("noteTitle").value.trim();
+    let content = document.getElementById("noteContent").value.trim();
+    if (!title && !content) return;
+  
+    let notes = JSON.parse(localStorage.getItem("notes")) || [];
+  
+    if (currentNoteIndex !== null) {
+      notes[currentNoteIndex] = { title, content };
+    } else {
+      notes.push({ title, content });
+      currentNoteIndex = notes.length - 1;
+    }
+  
+    localStorage.setItem("notes", JSON.stringify(notes));
+    loadNotes();
+  }
